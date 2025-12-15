e52dbfd98599398121c44d7a02132c083e5513d5
Fuat Kılıç <ftklc@outlook.com>
2025-12-15 19:59:37 +0300

    fear: add Paper/Rock/Scissors Game

---
+++ Paper-Rock-Scissors/app.js
@@ -0,0 +1,71 @@
+const playerResult = document.querySelector('.player__result h2')
+const cpuResult = document.querySelector('.cpu__result h2')
+const match = document.querySelector('.match')
+
+const btns = document.querySelectorAll('.btn')
+const playButton = document.querySelector('.play-button')
+const score = document.querySelector('.score__content h3')
+
+
+
+
+const option = ['paper', 'rock', 'scissors']
+let playerScore = 0
+let cpuScore = 0
+
+
+
+/* BUTTON ACTİVE */
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.parentNode.querySelector('.active').classList.remove('active')
+        btn.classList.add('active')
+
+        const buttonId = btn.dataset.id
+        playerResult.textContent = buttonId
+
+    })
+})
+
+
+
+
+
+const checkWinner = (player, cpu) => {
+    //Beraberlik
+    if (player === cpu) {
+        match.textContent = 'Draw'
+        return
+    }
+    //Oyuncu Kazanır ise
+    if (
+        (player === 'paper' && cpu === 'rock') ||
+        (player === 'rock' && cpu === 'scissors') ||
+        (player === 'scissors' && cpu === 'paper')
+    ) {
+        match.textContent = 'Player Win'
+        playerScore++
+    }
+    //Bilgisayar Kazarnı ise
+    else {
+        match.textContent = 'Cpu Win'
+        cpuScore++
+    }
+
+    score.textContent = `${playerScore} - ${cpuScore}`
+
+}
+playButton.addEventListener('click', () => {
+    let cpu = option[Math.floor(Math.random() * 3)]
+    let player = playerResult.textContent
+
+    if (!option.includes(player)) {
+        match.textContent = 'Lütfen Bir seçim yapınız'
+        return
+    }
+    setTimeout(() => {
+        cpuResult.textContent = cpu
+        checkWinner(player, cpu)
+    }, 300);
+
+})
