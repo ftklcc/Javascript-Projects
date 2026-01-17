91a68e13fc9dfc4684e84a897dab9a68e3f376b3
Fuat Kılıç <ftklc@outlook.com>
2026-01-17 11:16:35 +0300

    fix: JavaScript code has been optimized for a smoother transition.

--- FAQ-SSS/app.js
+++ FAQ-SSS/app.js
@@ -1,13 +1,35 @@
 const faqs = document.querySelectorAll('.faq')
 
-
 faqs.forEach(faq => {
-    faq.addEventListener('click', () => {
-        faqs.forEach(item => {
-            if (item !== faq) {
-                item.classList.remove('active')
-            }
-        })
+    const question = faq.querySelector('.question')
+    const answer = faq.querySelector('.answer')
+
+    question.addEventListener('click', () => {
+        const isActive = document.querySelector('.faq.active')
+
+        //if other faq is open a other faq closeed
+        if (isActive && isActive !== faq) {
+            isActive.classList.remove('active')
+            isActive.querySelector('.answer').style.height = 0
+        }
+        //open/close the clicked faq
         faq.classList.toggle('active')
+
+        //scrollHeight gives the actual height of the content
+        if (faq.classList.contains('active')) answer.style.height = `${answer.scrollHeight}px`
+        else answer.style.height = 0
+
     })
 })
+
+// faqs.forEach(faq => {
+//     faq.addEventListener('click', () => {
+//         faqs.forEach(item => {
+//             if (item !== faq) {
+//                 item.classList.remove('active')
+//             }
+//         })
+//         faq.classList.toggle('active')
+//     })
+// })
+
