6bb2551fa5774da03ecabb278b51198085ea1c89
Fuat Kılıç <ftklc@outlook.com>
2026-01-30 13:36:45 +0300

    fix:responsive design and all images change

--- MovieSlider/js/data.js
+++ MovieSlider/js/data.js
@@ -1,27 +1,27 @@
 export const movies = [
     {
-        img: '../img/game-of-thrones.webp',
+        img: '../img/got.webp',
         name: 'Game of Thrones',
         category: 'Bilim Kurgu',
         info: 'Game of Thrones, Westeros’un yedi hanedanının Demir Taht uğruna verdiği acımasız mücadeleyi anlatır. Güç, entrika ve ihanetin hükümsürdüğü bu dünyada affetmek zayıflık, hata ise ölümcül bir bedeldir.',
         year: ' 2011'
     },
     {
-        img: '../img/prison-break.webp',
+        img: '../img/prisonBreak.webp',
         name: 'Prison Break',
         category: 'Suç',
         info: 'Bir inşaat mühendisi, haksız yere suçlanan kardeşinin idamını engellemek için tasarımına yardımcı olduğu hapishaneye içeriden sızarak kaçış planı yapar.',
         year: '2005'
     },
     {
-        img: "../img/the-boys.webp",
+        img: "../img/theBoys.webp",
         name: 'The Boys',
         category: 'Bilim Kurgu',
         info: 'Haklının değil güçlünün söz sahibi olduğu bu düzende, ekip adaletin gerçekten ne anlama geldiğini herkese göstermeye kararlıdır.',
         year: '2019'
     },
     {
-        img: '../img/dexter-new-blood.webp',
+        img: '../img/dexter.webp',
         name: 'Dexter: New Blood',
         category: 'Suç',
         info: 'Gerilim ve psikolojik derinliğiyle dikkat çeken dizi, adalet ile vicdan arasındaki ince çizgiyi yeniden sorgulatıyor.',
